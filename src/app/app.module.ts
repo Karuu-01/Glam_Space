4c928db10f3f13f2e398d9af213f8ac5b066076c
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-24 16:27:43 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LivingRoomComponent } from './living-room/living-room.component';
+import { BedroomComponent } from './bedroom/bedroom.component';
+import { KitchenComponent } from './kitchen/kitchen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LivingRoomComponent,
+    BedroomComponent,
+    KitchenComponent
   ],
   imports: [
     BrowserModule,
